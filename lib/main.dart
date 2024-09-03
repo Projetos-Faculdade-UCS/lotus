0c0f8ba5fbb5281e709f2f4b8da2717cdf5b84f1
Marco Aurélio da Rosa Haubrich <marcolino.haubrich@gmail.com>
2024-09-02 21:24:33 -0300

    chore: Update project name to "Lotus"

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_desktop_template/src/app_widget.dart';
-import 'package:flutter_desktop_template/src/modules/app_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:lotus/src/app_widget.dart';
+import 'package:lotus/src/modules/app_module.dart';
 import 'package:menubar/menubar.dart';
 
 void main() async {
