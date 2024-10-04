62fd72ff4846d59dc6ad1edd6591b49fa475ebb4
Marco Haubrich <marcolino.haubrich@gmail.com>
2024-10-04 00:38:16 -0300

    refactor: :recycle: Update ComputadoresPage to use ComputadoresList widget
    - Removed unused import 'package:ativos_ui/ativos_ui.dart'
    - Added import 'package:lotus/src/modules/home/modules/computador/widgets/computadores_list.dart'
    - Replaced StreamBuilder with ComputadoresList widget to display computadores
    - Updated padding and added Miolo widget for consistent layout

--- lib/src/modules/home/modules/computador/pages/computadores_page.dart
+++ lib/src/modules/home/modules/computador/pages/computadores_page.dart
@@ -1,7 +1,8 @@
-import 'package:ativos_ui/ativos_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lotus/src/modules/home/modules/computador/bloc/computadores/computadores_bloc.dart';
+import 'package:lotus/src/modules/home/modules/computador/widgets/computadores_list.dart';
+import 'package:lotus_ui/lotus_ui.dart';
 
 /// {@template computadores_page}
 /// Página de computadores.
@@ -20,42 +21,29 @@ class ComputadoresPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => computadoresBloc,
-      child: BlocBuilder<ComputadoresBloc, ComputadoresState>(
-        builder: (context, state) {
-          if (state is ComputadoresLoading) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (state is ComputadoresSuccess) {
-            return StreamBuilder(
-              stream: state.computadores,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  final computadores = snapshot.data!;
-                  return ListView.builder(
-                    itemCount: computadores.length,
-                    itemBuilder: (context, index) {
-                      final computador = computadores[index];
-                      return AtivoCard(
-                        ativo: computador,
-                      );
-                    },
-                  );
-                } else {
-                  return const Center(
-                    child: Text('Nenhum computador encontrado.'),
-                  );
-                }
-              },
-            );
-          } else {
-            return const Center(
-              child: Text('Erro ao carregar computadores.'),
-            );
-          }
-        },
+    return Miolo(
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: BlocProvider(
+          create: (context) => computadoresBloc,
+          child: BlocBuilder<ComputadoresBloc, ComputadoresState>(
+            builder: (context, state) {
+              if (state is ComputadoresLoading) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (state is ComputadoresSuccess) {
+                return ComputadoresList(
+                  computadores: state.computadores,
+                );
+              } else {
+                return const Center(
+                  child: Text('Erro ao carregar computadores.'),
+                );
+              }
+            },
+          ),
+        ),
       ),
     );
   }
