5c2c083b74a07d07ae3a9b4e623af877ffbe43c4
Marco Haubrich <marhaubrich@ucs.br>
2024-10-04 17:39:44 -0300

    refactor: Update CustomSearchBar widget and SearchModal
    - Refactor CustomSearchBar widget to add a transition animation when tapped
    - Simplify the decoration of CustomSearchBar during the transition
    - Update SearchModal to use FadeTransition for smoother animation
    - Adjust the size and color of the SearchModal container
    - Add flightShuttleBuilder to Hero widget in SearchModal for better transition
    - Add placeholderBuilder to Hero widget in SearchModal to prevent layout shift
    - Create _Content widget to simulate Material's SearchBar in SearchModal

--- lib/src/modules/home/widgets/title_bar/search_bar/search_modal.dart
+++ lib/src/modules/home/widgets/title_bar/search_bar/search_modal.dart
@@ -6,30 +6,38 @@ import 'package:super_sliver_list/super_sliver_list.dart';
 /// Search modal.
 class SearchModal extends StatelessWidget {
   /// Default constructor.
-  const SearchModal({super.key});
+  const SearchModal({
+    super.key,
+  });
 
   static const int _itemCount = 2000000;
 
   static const _modalKey = ValueKey('search-modal');
 
+  static const _contentKey = ValueKey('search-content');
+
   /// Shows the search modal.
-  static Future<void> show() {
-    return Modular.to.push(
+  static Future<void> show() async {
+    await Future.delayed(const Duration(milliseconds: 100), () {});
+    return Modular.to.push<void>(
       PageRouteBuilder<void>(
         fullscreenDialog: true,
         opaque: false,
-        barrierColor: Colors.black.withOpacity(0.5),
+        barrierColor: Colors.black38,
         barrierDismissible: true,
         barrierLabel: 'search-modal',
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          return FadeTransition(
+            opacity: animation,
+            child: child,
+          );
+        },
         settings: const RouteSettings(
           name: 'search-modal',
         ),
         pageBuilder: (context, animation, secondaryAnimation) {
-          return FadeTransition(
-            opacity: animation,
-            child: const SearchModal(
-              key: _modalKey,
-            ),
+          return const SearchModal(
+            key: _modalKey,
           );
         },
       ),
@@ -38,18 +46,46 @@ class SearchModal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+    final theme = Theme.of(context);
+
     return Center(
       child: Container(
         decoration: BoxDecoration(
-          color: Theme.of(context).scaffoldBackgroundColor,
+          color: theme.scaffoldBackgroundColor,
           borderRadius: const BorderRadius.all(Radius.circular(10)),
         ),
-        width: MediaQuery.of(context).size.width * .5,
-        height: MediaQuery.of(context).size.height * .7,
+        width: size.width * .5,
+        height: size.height * .7,
         child: Column(
           children: [
             Hero(
               tag: 'search-bar',
+              flightShuttleBuilder: (
+                flightContext,
+                animation,
+                flightDirection,
+                fromHeroContext,
+                toHeroContext,
+              ) {
+                return Container(
+                  height: 40,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey.withOpacity(.9)),
+                    borderRadius: BorderRadius.circular(10),
+                    color: theme.scaffoldBackgroundColor,
+                  ),
+                  child: const _Content(
+                    key: _contentKey,
+                  ),
+                );
+              },
+              placeholderBuilder: (context, size, widget) {
+                return SizedBox(
+                  width: size.width,
+                  height: size.height,
+                );
+              },
               child: SearchBar(
                 autoFocus: true,
                 elevation: const WidgetStatePropertyAll(0),
@@ -90,6 +126,21 @@ class SearchModal extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _Content extends StatelessWidget {
+  const _Content({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // simulates a material's SearchBar
+    return const Material(
+      color: Colors.transparent,
+      child: SearchBar(
+        elevation: WidgetStatePropertyAll(0),
       ),
     );
   }
