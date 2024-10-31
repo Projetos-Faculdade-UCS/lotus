b741a1023c9dae1eee84a9c4039d101f47a58b43
Marco Haubrich <marhaubrich@ucs.br>
2024-10-31 12:53:17 -0300

    Refactor: Change entries and menu to use getter methods in Menu widget; update darkShadow color definition

--- lib/src/modules/home/widgets/title_bar/menu/menu.dart
+++ lib/src/modules/home/widgets/title_bar/menu/menu.dart
@@ -17,68 +17,68 @@ class Menu extends StatefulWidget {
 
 class _MenuState extends State<Menu> {
   // define your context menu entries
-  late final entries = <ContextMenuEntry>[
-    MenuItem<void>(
-      label: 'Ativar admin',
-      icon: HugeIcons.strokeRoundedShield02,
-      onSelected: () {
-        debugPrint('Ativar admin');
-      },
-    ),
-    MenuItem<void>(
-      label: 'Mudar Tema',
-      icon: HugeIcons.strokeRoundedMoon,
-      onSelected: () {
-        AdaptiveTheme.of(context).toggleThemeMode(useSystem: false);
-      },
-    ),
-    MenuDivider(
-      thickness: 1,
-      height: 1,
-      color: Theme.of(context).dividerColor.withOpacity(0.5),
-    ),
-    MenuItem<void>(
-      label: 'Sobre',
-      icon: HugeIcons.strokeRoundedInformationCircle,
-      onSelected: () {
-        showAboutDialog(
-          context: context,
-          applicationIcon: const SizedBox(
-            width: 48,
-            height: 48,
-            child: LotusIcon(),
-          ),
-          applicationName: 'Lotus',
-          applicationVersion: '1.0.0',
-          applicationLegalese:
-              '© $year Universidade de Caxias do Sul. All rights reserved.',
-        );
-      },
-    ),
-  ];
+  List<ContextMenuEntry> get entries => <ContextMenuEntry>[
+        MenuItem<void>(
+          label: 'Ativar admin',
+          icon: HugeIcons.strokeRoundedShield02,
+          onSelected: () {
+            debugPrint('Ativar admin');
+          },
+        ),
+        MenuItem<void>(
+          label: 'Mudar Tema',
+          icon: HugeIcons.strokeRoundedMoon,
+          onSelected: () {
+            AdaptiveTheme.of(context).toggleThemeMode(useSystem: false);
+          },
+        ),
+        MenuDivider(
+          thickness: 1,
+          height: 1,
+          color: Theme.of(context).dividerColor.withOpacity(0.5),
+        ),
+        MenuItem<void>(
+          label: 'Sobre',
+          icon: HugeIcons.strokeRoundedInformationCircle,
+          onSelected: () {
+            showAboutDialog(
+              context: context,
+              applicationIcon: const SizedBox(
+                width: 48,
+                height: 48,
+                child: LotusIcon(),
+              ),
+              applicationName: 'Lotus',
+              applicationVersion: '1.0.0',
+              applicationLegalese:
+                  '© $year Universidade de Caxias do Sul. All rights reserved.',
+            );
+          },
+        ),
+      ];
 
   final year = DateTime.now().year;
 
   // initialize a context menu
-  late final menu = ContextMenu(
-    entries: entries,
-    position: const Offset(0, 36),
-    // padding: const EdgeInsets.all(8),
-    borderRadius: const BorderRadius.only(
-      bottomLeft: Radius.circular(8),
-      bottomRight: Radius.circular(8),
-    ),
-    boxDecoration: BoxDecoration(
-      color: Theme.of(context).canvasColor,
-      boxShadow: [
-        BoxShadow(
-          color: Theme.of(context).shadowColor.withOpacity(0.2),
-          blurRadius: 4,
-          offset: const Offset(0, 4),
+  ContextMenu get menu => ContextMenu(
+        entries: entries,
+        position: const Offset(0, 36),
+        // padding: const EdgeInsets.all(8),
+        borderRadius: const BorderRadius.only(
+          bottomLeft: Radius.circular(8),
+          bottomRight: Radius.circular(8),
         ),
-      ],
-    ),
-  );
+        boxDecoration: BoxDecoration(
+          color: Theme.of(context).canvasColor,
+          boxShadow: [
+            BoxShadow(
+              color: Theme.of(context).shadowColor.withOpacity(0.2),
+              blurRadius: 4,
+              offset: const Offset(0, 4),
+            ),
+          ],
+        ),
+      );
 
   @override
   Widget build(BuildContext context) {
