0768c8bd238f15ded09f24a469d1ad510dcaec90
Marco Aurélio da Rosa Haubrich <marcolino.haubrich@gmail.com>
2024-11-19 23:40:14 -0300

    fix: Update LotusIcon to use PNG asset and improve sizing, adjust hover color opacity in SidebarHeader

--- lib/src/lotus_icon.dart
+++ lib/src/lotus_icon.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:vector_graphics/vector_graphics_compat.dart';
 
 /// A widget that displays the Lotus icon.
 class LotusIcon extends StatelessWidget {
@@ -13,12 +11,19 @@ class LotusIcon extends StatelessWidget {
   /// The size of the icon.
   final double? size;
 
-  static const Widget _svg = SvgPicture(
-    AssetBytesLoader('assets/logo/ucs-logo.svg.vec'),
+  static final Widget _svg = Image.asset(
+    'assets/logo/lotus_icon.png',
+    cacheHeight: 512,
+    cacheWidth: 512,
+    isAntiAlias: true,
   );
 
   @override
   Widget build(BuildContext context) {
-    return _svg;
+    return SizedBox(
+      width: double.infinity,
+      height: size,
+      child: _svg,
+    );
   }
 }
