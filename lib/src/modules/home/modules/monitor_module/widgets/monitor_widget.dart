f6e7ecbf842aceeeab7bdaa9bcaf58c5e0d2f2bf
Bruno Martins <bgmartins@ucs.br>
2024-11-22 18:05:10 -0300

    feat: :sparkles: Conclusão do detalhe de um monitor

--- lib/src/modules/home/modules/monitor_module/widgets/monitor_widget.dart
+++ lib/src/modules/home/modules/monitor_module/widgets/monitor_widget.dart
@@ -1,6 +1,8 @@
 import 'package:ativos_ui/src/widgets/cabecalho_ativo.dart';
 import 'package:flutter/material.dart' hide BoxDecoration, BoxShadow;
 import 'package:flutter_inset_box_shadow/flutter_inset_box_shadow.dart';
+import 'package:hugeicons/hugeicons.dart';
+import 'package:lotus/src/modules/home/modules/computador/widgets/detalhe_computador/card_local.dart';
 import 'package:repositories/repositories.dart';
 
 class MonitorWidget extends StatelessWidget {
@@ -43,6 +45,87 @@ class MonitorWidget extends StatelessWidget {
               computadorRelacionamentos: monitor.relacionamentos,
               computadorResponsavel: monitor.responsavel,
               tipoAtivo: monitor.tipo,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 24,
+                vertical: 24,
+              ),
+              child: Row(
+                children: [
+                  Flexible(
+                    flex: 2,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            HugeIcon(
+                              icon: HugeIcons.strokeRoundedMapsLocation01,
+                              color: colorScheme.onSurfaceVariant,
+                              size: 20,
+                            ),
+                            const SizedBox(width: 4),
+                            Text(
+                              'Local atual',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: colorScheme.onSurfaceVariant,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 4),
+                        CardLocal(
+                          sala: monitor.sala,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 24),
+                  Flexible(
+                    flex: 2,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            HugeIcon(
+                              icon: HugeIcons.strokeRoundedClock04,
+                              color: colorScheme.onSurfaceVariant,
+                              size: 20,
+                            ),
+                            const SizedBox(width: 4),
+                            Flexible(
+                              fit: FlexFit.tight,
+                              child: Text(
+                                'Histórico de movimentações',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: colorScheme.onSurfaceVariant,
+                                ),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const Expanded(
+                          child: Placeholder(),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
